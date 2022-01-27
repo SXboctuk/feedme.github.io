ef11085c8944bccaff27a05c74e34da944ce547a
Maksim Lisitski <stargate.xboctuk@gmail.com>
2022-01-27 21:35:59 +0300

    add logic header search

--- src/components/HeaderSearch/HeaderSearch.Styled.ts
+++ src/components/HeaderSearch/HeaderSearch.Styled.ts
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import styles from '../../constants/stylesProperty';
 import SvgSearch from '../Svg/SvgSearch';
 
 export const InputSearchIcon = styled(SvgSearch)`
 	position: absolute;
+	z-index: 3;
 	top: calc(50% - ${styles.distances.xs});
 	left: 15px;
 `;
@@ -14,7 +16,30 @@ export const InputContainer = styled.div`
 		display: none;
 	}
 `;
+export const InputSuggestionItemWrapper = styled.div`
+	position: absolute;
+	z-index: 1;
+	width: 100%;
+	border-radius: ${({ theme }) => theme.borderRadius};
+	background-color: ${({ theme }) => theme.colors.grayDark};
+	bottom: 0px;
+	transform: translateY(80%);
+	padding: ${styles.distances.mdPlus} ${styles.distances.xs}
+		${styles.distances.xs} ${styles.distances.xs};
+`;
+export const InputSuggestionItem = styled(Link)`
+	display: block;
+	width: 100%;
+	padding: ${styles.distances.xs};
+	border-radius: ${({ theme }) => theme.borderRadius};
+	cursor: pointer;
+	&:hover {
+		background-color: ${({ theme }) => theme.colors.gray};
+	}
+`;
 export const Input = styled.input`
+	z-index: 2;
+	position: relative;
 	height: ${({ height }) => (height ? height : '40px')};
 	font-size: ${({ size }) => (size ? size : 'inherit')};
 	width: 100%;
--- src/components/HeaderSearch/HeaderSearch.tsx
+++ src/components/HeaderSearch/HeaderSearch.tsx
@@ -1,11 +1,69 @@
-import React from 'react';
-import { InputContainer, InputSearchIcon, Input } from './HeaderSearch.Styled';
+import React, { useEffect, useState } from 'react';
+import { regexString } from '../../constants/regex';
+import { routePath } from '../../constants/routePath';
+import {
+	InputContainer,
+	InputSearchIcon,
+	Input,
+	InputSuggestionItem,
+	InputSuggestionItemWrapper,
+} from './HeaderSearch.Styled';
 
+interface ISearchUser {
+	userName: string;
+	id: string;
+}
 const HeaderSearch = () => {
+	const [searchValue, setSearchValue] = useState('');
+	const [users, setUsers] = useState<ISearchUser[]>([]);
+	const [suggestion, setSuggestion] = useState<ISearchUser[]>([]);
+
+	const handlerSuggestion = (text: string) => {
+		let matches: ISearchUser[] = [];
+		if (text.length > 0) {
+			matches = users.filter((elem) => {
+				const regex = new RegExp(`${text}`, 'gi');
+				return elem.userName.match(regex);
+			});
+		}
+		setSuggestion(matches);
+		console.log(suggestion);
+	};
+	const handlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.value.match(regexString.IS_VALID_STRING) !== null) {
+			setSearchValue(e.target.value);
+			handlerSuggestion(searchValue);
+		}
+	};
+
+	useEffect(() => {
+		//fetch users
+		setUsers([
+			{ userName: 'Jon', id: '1' },
+			{ userName: 'Ivan', id: '2' },
+			{ userName: 'Max', id: '3' },
+			{ userName: 'Vlad ster', id: '4' },
+			{ userName: 'ConstantUser', id: '123' },
+		]);
+	}, []);
+
 	return (
 		<InputContainer>
-			<Input />
+			<Input onChange={handlerInput} value={searchValue} />
 			<InputSearchIcon />
+			{suggestion.length > 0 && searchValue.length > 0 ? (
+				<InputSuggestionItemWrapper>
+					{suggestion.slice(0, 3).map((elem, i) => (
+						<InputSuggestionItem
+							key={elem.userName + elem.id + i}
+							to={routePath.PROFILE + elem.id}
+							onClick={() => setSearchValue('')}
+						>
+							{elem.userName}
+						</InputSuggestionItem>
+					))}
+				</InputSuggestionItemWrapper>
+			) : null}
 		</InputContainer>
 	);
 };
